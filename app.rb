5ec455f96679a12adcdb6caa55d8c89fcfb73923
Matt Hughes <matthughes.tech@gmail.com>
2015-07-23 17:17:09 -0400

    Update routes for multiple rolls

--- app.rb
+++ app.rb
@@ -2,43 +2,94 @@ require('sinatra')
 require('sinatra/json')
 require_relative('lib/roller.rb')
 
-dice = Roller.new
-
 before do
   content_type :json
 end
 
-get '/d4' do
-  roll = dice.roll(4)
-  json roll
+get '/d4/?:num?' do
+  num = number_to_roll(params[:num])
+  roll = roll_results(num, 4)
+  total = roll_total(roll)
+
+  json :roll => roll, :total => total
 end
 
-get '/d6' do
-  roll = dice.roll(6)
-  json roll
+get '/d6/?:num?' do
+  num = number_to_roll(params[:num])
+  roll = roll_results(num, 6)
+  total = roll_total(roll)
+
+  json :roll => roll, :total => total
 end
 
-get '/d8' do
-  roll = dice.roll(8)
-  json roll
+get '/d8/?:num?' do
+  num = number_to_roll(params[:num])
+  roll = roll_results(num, 8)
+  total = roll_total(roll)
+
+  json :roll => roll, :total => total
 end
 
-get '/d10' do
-  roll = dice.roll(10)
-  json roll
+get '/d10/?:num?' do
+  num = number_to_roll(params[:num])
+  roll = roll_results(num, 10)
+  total = roll_total(roll)
+
+  json :roll => roll, :total => total
 end
 
-get '/d12' do
-  roll = dice.roll(12)
-  json roll
+get '/d12/?:num?' do
+  num = number_to_roll(params[:num])
+  roll = roll_results(num, 12)
+  total = roll_total(roll)
+
+  json :roll => roll, :total => total
 end
 
-get '/d20' do
-  roll = dice.roll(20)
-  json roll
+get '/d20/?:num?' do
+  num = number_to_roll(params[:num])
+  roll = roll_results(num, 20)
+
+  high = roll.max
+  low = roll.min
+
+  json :roll => roll, :high => high, :low => low
 end
 
-get '/d100' do
-  roll = dice.roll(100)
-  json roll
+get '/d100/?:num?' do
+  num = number_to_roll(params[:num])
+  roll = roll_results(num, 100)
+
+  high = roll.max
+  low = roll.min
+
+  json :roll => roll, :high => high, :low => low
+end
+
+private
+def number_to_roll(input)
+  num = 0
+  num = input.to_i unless input.nil?
+
+  num
+end
+
+def roll_results(num, type)
+  dice = Roller.new
+  output = []
+
+  num.times do
+    output.push(dice.roll(type))
+  end
+
+  output
+end
+
+def roll_total(rolls)
+  sum = 0
+  rolls.each do |r|
+    sum += r
+  end
+
+  sum
 end
